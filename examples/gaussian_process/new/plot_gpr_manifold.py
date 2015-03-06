0766e688b544a8e8851a68d450e94326d7242905
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-03-06 13:57:47 +0100

    DOC Revised example of illustrating usage of Manifold kernel

--- examples/gaussian_process/new/plot_gpr_manifold.py
+++ examples/gaussian_process/new/plot_gpr_manifold.py
@@ -1,3 +1,9 @@
+"""Gaussian process regression (GPR) with manifold kernel. """
+print __doc__
+
+# Authors: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+#
+# License: BSD 3 clause
 
 import numpy as np
 
@@ -15,26 +21,29 @@ np.random.seed(0)
 n_samples = 100
 n_features = 5
 n_dim_manifold = 2
+n_hidden = 3
 
 # Generate data
-def f(X_nn):
+def f(X_nn):  # target function
+    return np.sqrt(np.abs(X_nn[:, 0] * X_nn[:, 1]))
     #return X_nn[:, 0] * X_nn[:, 1]
-    return X_nn[:, 0]**2 + X_nn[:, 1]**2
+    #return X_nn[:, 0]**2 + X_nn[:, 1]**2
 
-X = np.random.uniform(-5, 5, (n_samples, n_features))
-A = np.random.random((n_features, n_dim_manifold))
-X_nn = X.dot(A)
-y = f(X_nn)
+X_ = np.random.uniform(-5, 5, (n_samples, n_dim_manifold))  # data on manifold
+A = np.random.random((n_dim_manifold, n_features))  # mapping from manifold to data space
+X = X_.dot(A)
+y = f(X_)  # Generate target values by applying function to manifold
 
-# Standard Gaussian Process
+# Gaussian Process with anisotropic RBF kernel
 kernel = (1e-10, 1.0, 100) * RBF([(0.1, 1, 100.0) for i in range(n_features)])
 gp = GaussianProcessRegressor(kernel=kernel)
 
-# Manifold Gaussian Process
+# Gaussian Process with Manifold kernel (using an isotropic RBF kernel on
+# manifold for learning the target function)
 kernel_nn = (1e-10, 1.0, 100) \
-    * ManifoldKernel(base_kernel=RBF((1.0, 5.0, 100.0)),
-                     architecture=((n_features, n_dim_manifold),),
-                     transfer_fct="linear", max_nn_weight=0.1) \
+    * ManifoldKernel(base_kernel=RBF((0.1, 1.0, 100.0)),
+                     architecture=((n_features, n_hidden, n_dim_manifold),),
+                     transfer_fct="tanh", max_nn_weight=2.5) \
     + WhiteKernel((1e-10, 1e-3, 1e-1))
 gp_nn = GaussianProcessRegressor(kernel=kernel_nn, y_err=0)
 
@@ -42,23 +51,31 @@ gp_nn = GaussianProcessRegressor(kernel=kernel_nn, y_err=0)
 gp.fit(X, y)
 gp_nn.fit(X, y)
 
-X_test = np.random.uniform(-5, 5, (1000, n_features))
-X_nn_test = X_test.dot(A)
-y_test = f(X_nn_test)
+print "Initial hyperparameters: %s" % gp_nn.kernel.params
+print "Log-marginal-likelihood: %s" \
+    % gp_nn.log_marginal_likelihood(gp_nn.kernel.params)
+
+print "Learned hyperparameters: %s" % gp_nn.theta_
+print "Log-marginal-likelihood: %s" \
+    % gp_nn.log_marginal_likelihood(gp_nn.theta_)
+
+X_test_ = np.random.uniform(-5, 5, (1000, n_dim_manifold))
+X_nn_test = X_test_.dot(A)
+y_test = f(X_test_)
 plt.figure(0)
-plt.scatter(y_test, gp.predict(X_test), c='b', label="GP RBF")
-plt.scatter(y_test, gp_nn.predict(X_test), c='r', label="GP NN")
+plt.scatter(y_test, gp.predict(X_nn_test), c='b', label="GP RBF")
+plt.scatter(y_test, gp_nn.predict(X_nn_test), c='r', label="GP NN")
 plt.xlabel("True")
-plt.xlabel("Predicted")
+plt.ylabel("Predicted")
 plt.legend(loc=0)
 plt.title("Scatter plot on test data")
 
 plt.figure(1)
-X_gp_nn_test = gp_nn.kernel_.k1.k2._project_manifold(X_test)
+X_gp_nn_test = gp_nn.kernel_.k1.k2._project_manifold(X_nn_test)
 plt.scatter(X_gp_nn_test[:, 0], X_gp_nn_test[:, 1], c=y_test)
 plt.colorbar()
 plt.title("Learned 2D Manifold")
-
+plt.show()
 
 # Plot learning curve
 def plot_learning_curve(estimators, title, X, y, cv=None,
