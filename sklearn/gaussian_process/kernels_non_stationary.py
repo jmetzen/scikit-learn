6749a7df3c95bc8dece0ca0aae4e51a77cd46575
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-03-12 11:10:33 +0100

    ENH Adding LocalLengthScalesKernel

--- sklearn/gaussian_process/kernels_non_stationary.py
+++ sklearn/gaussian_process/kernels_non_stationary.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from scipy.special import gamma, kv
+
 from sklearn.gaussian_process.kernels import Kernel, _approx_fprime
 
 
@@ -155,3 +157,182 @@ def determine_network_layout(architecture):
         n_outputs += subnet[-1]
 
     return n_outputs, n_params
+
+
+class LocalLengthScalesKernel(Kernel):
+    """ Non-stationary kernel based on local smoothness estimates.
+
+    This non-stationary correlation model learns internally point estimates of
+    local smoothness using a second-level Gaussian Process. For this, it
+    selects a subset of the training data and learns length-scales at this
+    specific points. These length scales are generalized using the second-level
+    Gaussian Process. Furthermore, global (isotropic or anisotropic) length
+    scales are learned for both the top-level GP and the length-scale GP.
+
+    The kernel of the second-level GP is an RBF kernel.
+
+    Parameters
+    ----------
+    isotropic : bool, default=True
+        Whether the global length-scales of the top-level GP are isotropic or
+        anisotropic
+
+    l_isotropic : bool, default=True
+        Whether the global length-scales of the length-scale GP are isotropic
+        or anisotropic
+
+    l_samples: int, default=10
+        How many datapoints from the training data are selected as support
+        points for learning the length-scale GP
+
+    .. seealso::
+
+    "Nonstationary Gaussian Process Regression using Point Estimates of Local
+    Smoothness", Christian Plagemann, Kristian Kersting, and Wolfram Burgard,
+    ECML 2008
+    """
+
+    def __init__(self, n_dims,
+                 isotropic=True, theta0=1e-1, thetaL=1e-3, thetaU=1.0,
+                 l_isotropic=True, theta_l_0=1e-1, theta_l_L=1e-3, theta_l_U=1e1,
+                 l_samples=10, l_0=1.0, l_L=0.1, l_U=10.0, X_=None):
+        self.n_dims = n_dims
+        self.isotropic = isotropic
+        self.l_isotropic = l_isotropic
+        self.l_samples = l_samples
+        self.X_ = np.asarray(X_)
+        if self.X_ is not None:
+            assert self.X_.shape[0] == self.l_samples
+        assert self.X_ is not None  # XXX
+
+        # Determine how many entries in theta belong to the different
+        # categories (used later for parsing theta)
+        self.theta_gp_size = 1 if self.isotropic else self.n_dims
+        self.theta_l_size = 1 if self.l_isotropic else self.n_dims
+        self.theta_size = \
+            self.theta_gp_size + self.theta_l_size + self.l_samples
+
+        theta0 = [theta0] * self.theta_gp_size
+        thetaL = [thetaL] * self.theta_gp_size
+        thetaU = [thetaU] * self.theta_gp_size
+
+        theta0 += [theta_l_0] * self.theta_l_size
+        thetaL += [theta_l_L] * self.theta_l_size
+        thetaU += [theta_l_U] * self.theta_l_size
+
+        theta0 += [l_0] * self.l_samples
+        thetaL += [l_L] * self.l_samples
+        thetaU += [l_U] * self.l_samples
+
+        param_space = np.vstack((thetaL, theta0, thetaU)).T
+
+        self._parse_param_space(param_space)
+
+    @property
+    def params(self):
+        return self.theta
+
+    @params.setter
+    def params(self, theta):
+        from . import GaussianProcessRegressor
+        from .kernels import RBF
+
+        self.theta = np.asarray(theta, dtype=np.float)
+
+        # Parse theta into its components
+        self.theta_gp, self.theta_l, self.length_scales = \
+            self._parse_theta(self.theta)
+
+        # Train length-scale Gaussian Process
+        kernel = RBF(np.atleast_2d(self.theta_l))
+        self.gp_l = GaussianProcessRegressor(kernel=kernel)
+        self.gp_l.fit(self.X_, np.log10(self.length_scales))
+
+    def __call__(self, X, Y=None, eval_gradient=False):
+        l_train = 10 ** self.gp_l.predict(X)
+
+        # Prepare distances and length scale information for any pair of
+        # datapoints, whose correlation shall be computed
+        if Y is not None:
+            # Get pairwise componentwise L1-differences to the input training
+            # set
+            d = Y[:, np.newaxis, :] - X[np.newaxis, :, :]
+            d = d.reshape((-1, Y.shape[1]))
+            # Predict length scales for query datapoints
+            l_query = 10 ** self.gp_l.predict(Y)
+            l = np.transpose([np.tile(l_train, len(l_query)),
+                              np.repeat(l_query, len(l_train))])
+        else:
+            # No external datapoints given; auto-correlation of training set
+            # is used instead
+            d = X[:, np.newaxis, :] - X[np.newaxis, :, :]
+            d = d.reshape((-1, X.shape[1]))
+            l = np.transpose([np.tile(l_train, len(l_train)),
+                              np.repeat(l_train, len(l_train))])  # XXX: check
+
+        # Compute general Matern kernel
+        nu = 1.5
+        if d.ndim > 1 and self.theta_gp.size == d.ndim:
+            activation = \
+                np.sum(self.theta_gp.reshape(1, d.ndim) * d ** 2, axis=1)
+        else:
+            activation = self.theta_gp[0] * np.sum(d ** 2, axis=1)
+        tmp = 0.5 * (l**2).sum(1)
+        tmp2 = np.maximum(2*np.sqrt(nu * activation / tmp), 1e-5)
+        k = np.sqrt(l[:, 0]) * np.sqrt(l[:, 1]) / (gamma(nu) * 2**(nu - 1))
+        k /= np.sqrt(tmp)
+        k *= tmp2**nu * kv(nu, tmp2)
+
+        assert np.all(np.isfinite(k))
+
+        # Convert correlations to 2d matrix
+        if Y is not None:
+            return k.reshape(-1, X.shape[0]).T
+        else:  # exploit symmetry of auto-correlation
+            K = k.reshape(X.shape[0], X.shape[0])
+            if not eval_gradient:
+                return K
+            else:
+                # approximate gradient numerically
+                def f(params):  # helper function
+                    import copy  # XXX: Avoid deepcopy
+                    kernel = copy.deepcopy(self)
+                    kernel.params = params
+                    return kernel(X)
+                return K, _approx_fprime(self.params, f, 1e-5)
+
+    def _parse_theta(self, theta):
+        """ Parse parameter vector theta into its components.
+
+        Parameters
+        ----------
+        theta : array_like
+            An array containing all hyperparameters.
+
+        Returns
+        -------
+        theta_gp : array_like
+            An array containing the hyperparameters of the main GP.
+        theta_l : array_like
+            An array containing the hyperparameters of the length-scale GP.
+        length_scales : array_like
+            An array containing the length-scales for the length-scale GP.
+        """
+        theta = np.asarray(theta, dtype=np.float)
+
+        assert (theta.size == self.theta_size), \
+            "theta does not have the expected size (expected: %d, " \
+            "actual size %d). Expected: %d entries for main GP, " \
+            "%d entries for length-scale GP, %d entries containing the "\
+            "length scales, and %d entries for nu." \
+            % (self.theta_size, theta.size, self.theta_gp_size,
+               self.theta_l_size, self.l_samples, self.nu_size)
+
+        # Split theta in its components
+        theta_gp = theta[:self.theta_gp_size]
+        theta_l = \
+            theta[self.theta_gp_size:][:self.theta_l_size]
+        length_scales = \
+            theta[self.theta_gp_size+self.theta_l_size:][:self.l_samples]
+
+        return theta_gp, theta_l, length_scales
